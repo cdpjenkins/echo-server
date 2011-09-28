c891510ac55e5aab72e5d05303b89811b37d1fcd
cdpjenkins <cdpjenkins@gmail.com>
2011-09-28 21:09:24 +0100

    Echo server seems to be working with Clojure 1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,7 @@
 (defproject echo-server "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                   [org.clojure/clojure-contrib "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+
+  :dev-dependencies [[swank-clojure "1.3.2"]]
+  )
--- src/echo_server/core.clj
+++ src/echo_server/core.clj
@@ -1,1 +1,20 @@
-(ns echo-server.core)
+(ns echo-server.core
+  (:use clojure.contrib.server-socket))
+
+(import
+ '(java.io InputStreamReader BufferedReader PrintWriter)
+ '(java.net InetAddress ServerSocket))
+
+(defn echo-protocol [in out]
+  (binding [*in* (->> in InputStreamReader. BufferedReader.)
+            *out* (->> out PrintWriter.)]
+    (loop []
+      (let [line ( read-line)]
+        (println line)
+        (when ( not= line "quit")
+          (recur)))))
+  )
+
+(defn make-server []
+  (println "skank-ston")
+  (create-server 1234 echo-protocol))
